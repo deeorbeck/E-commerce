73a847314fc4da9914b628a1e7954bf69fbc234a
Portfoliosuz <85097020+Portfoliosuz@users.noreply.github.com>
2021-06-14 22:17:48 +0300

    removed secrets

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#nd^$sd*#y#ccxfb&w(t02c+==)q)ds%43fq(n+1etn#0mr8t@'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -130,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'myportfoliosuzb@gmail.com'
-EMAIL_HOST_PASSWORD = 'Portfolios362003@'
+EMAIL_HOST_USER = 'Siz yoki korxona emaili'
+EMAIL_HOST_PASSWORD = 'Email paroli'
